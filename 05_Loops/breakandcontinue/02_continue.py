ca069fedcc47918d119b03a927b3af94535f33c6
Binod Dey <binoddey06@gmail.com>
2026-01-27 21:38:40 +0530

    Added Loops - Python

---
+++ 05_Loops/breakandcontinue/02_continue.py
@@ -0,0 +1,17 @@
+i = 0
+while i<10 :
+    i+=1
+    if i==5:
+        # i+=1
+        continue
+    print(i)
+    
+  
+i = 0
+while i<10 :
+    # i+=1
+    if i==5:
+        i+=1
+        continue
+    print(i)  
+    i+=1
---
+++ 05_Loops/while loop/01_while.py
@@ -0,0 +1,6 @@
+i = 1 #iterator
+
+while i<=10: #condition
+
+    print("Hello", i)
+    i+=1 #increment
